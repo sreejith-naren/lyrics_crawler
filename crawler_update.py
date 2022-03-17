8a19401b92fe540426f986da517db95debf03012
sreejith-naren <sreejith@hamon.in>
2022-03-17 13:23:48 +0530

    added get_songs

--- crawler_update.py
+++ crawler_update.py
@@ -11,3 +11,15 @@ def get_artists(url):
     for i in links:
         ret.append((i.text,i['href']))
     return ret
+
+def get_songs(artist_url):
+    songs=[]
+    r = requests.get(artist_url)
+    body = r.content
+    soup = BeautifulSoup(body, features="html.parser")
+    tracklists = soup.find("table", {"class" : "tracklist"})
+    links=tracklists.find_all("a")
+    for i in links:
+        songs.append((i.text,i['href']))
+    return songs    
+
